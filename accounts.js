11fd9fd5f2c2ae404a3f4e5c77304b3b16d39920
rustamabdullin <r.m.abdullin@gmail.com>
2021-06-13 00:26:31 +0300

    Hardcoded wallet pub and private keys

--- accounts.js
+++ accounts.js
@@ -1,14 +1,14 @@
 export default {
     "liquidator": [{
-        adr: "",
-        pKey: ""
+        adr: "0x9Af00e58040f2F0FBfb3aCd542F7C5f1A4FAbD70",
+        pKey: "dde2ec361acec024e78587f605fb8f2f098aacb5c492393e7cca66a42f288664"
     }],
     "rollover": [{
-        adr: "",
-        pKey: ""
+        adr: "0x9Af00e58040f2F0FBfb3aCd542F7C5f1A4FAbD70",
+        pKey: "dde2ec361acec024e78587f605fb8f2f098aacb5c492393e7cca66a42f288664"
     }],
     "arbitrage": [{
-        adr: "",
-        pKey: ""
+        adr: "0x9Af00e58040f2F0FBfb3aCd542F7C5f1A4FAbD70",
+        pKey: "dde2ec361acec024e78587f605fb8f2f098aacb5c492393e7cca66a42f288664"
     }],
 }
