1521bc7ee893233c52e7c20ea89b66eaec1ee0e0
batkailyas <ilislyamov@gmail.com>
2021-06-18 04:25:16 +0600

    Update accounts.js
    Changed pKey to ks. Changed values to replaceable words

--- accounts.js
+++ accounts.js
@@ -1,14 +1,14 @@
 export default {
     "liquidator": [{
-        adr: "0x9Af00e58040f2F0FBfb3aCd542F7C5f1A4FAbD70",
-        pKey: "dde2ec361acec024e78587f605fb8f2f098aacb5c492393e7cca66a42f288664"
+        adr: "ADDR",
+        ks: PRIVATE
     }],
     "rollover": [{
-        adr: "0x9Af00e58040f2F0FBfb3aCd542F7C5f1A4FAbD70",
-        pKey: "dde2ec361acec024e78587f605fb8f2f098aacb5c492393e7cca66a42f288664"
+        adr: "ADDR",
+        ks: PRIVATE
     }],
     "arbitrage": [{
-        adr: "0x9Af00e58040f2F0FBfb3aCd542F7C5f1A4FAbD70",
-        pKey: "dde2ec361acec024e78587f605fb8f2f098aacb5c492393e7cca66a42f288664"
+        adr: "ADDR",
+        ks: PRIVATE
     }],
 }
